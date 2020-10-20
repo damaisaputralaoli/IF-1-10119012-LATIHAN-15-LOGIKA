fb00be4c3da1a5dc731d7148db2a39f888d20577
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-10-20 21:46:51 +0700

    Add files via upload

---
+++ Latihan15pbo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan15pbo;
+
+/**
+ *
+ * @author damai
+ */
+public class Latihan15pbo {
+
+    public static void main (String[] args){
+    boolean a =true;
+    boolean b =false;
+    System.out.println("a && b = " + (a && b));
+    System.out.println("a || b = " + (a || b));
+    System.out.println("!a = " + !a);
+    System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
